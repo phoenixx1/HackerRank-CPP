4e1280c63fe760603dfc631bde2eb3366333946c
phoenixx1 <xphoenix1010@gmail.com>
2020-03-08 00:47:50 +0530

    Initial commit

--- STL/Print Pretty.cpp
+++ STL/Print Pretty.cpp
@@ -14,7 +14,7 @@ int main() {
 		double C; cin >> C;
         cout  << nouppercase << left << hex << showbase << (long)A << endl;
         cout << fixed << setfill('_') << setw(0xf) << right << showpos << noshowbase << setprecision(2) << B << endl;
-        cout << nouppercase << scientific << noshowpos << setprecision(9) << C << endl;
+        cout << uppercase << scientific << noshowpos << setprecision(9) << C << endl;
 	}
 	return 0;
 }
