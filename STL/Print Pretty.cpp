85a73898c76684ed6adf0fe8ea971c501667f72f
phoenixx1 <xphoenix1010@gmail.com>
2020-03-08 00:45:57 +0530

    Initial commit

---
+++ STL/Print Pretty.cpp
@@ -0,0 +1,20 @@
+//author @Nishant
+
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+	int T; cin >> T;
+	cout << setiosflags(ios::uppercase);
+	cout << setw(0xf) << internal;
+	while(T--) {
+		double A; cin >> A;
+		double B; cin >> B;
+		double C; cin >> C;
+        cout  << nouppercase << left << hex << showbase << (long)A << endl;
+        cout << fixed << setfill('_') << setw(0xf) << right << showpos << noshowbase << setprecision(2) << B << endl;
+        cout << nouppercase << scientific << noshowpos << setprecision(9) << C << endl;
+	}
+	return 0;
+}
