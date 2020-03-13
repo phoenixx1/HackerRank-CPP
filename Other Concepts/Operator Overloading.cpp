9c26654a431eb1184c854993e8360dd13af5eacb
phoenixx1 <xphoenix1010@gmail.com>
2020-03-14 00:43:40 +0530

    Initial commit

---
+++ Other Concepts/Operator Overloading.cpp
@@ -0,0 +1,68 @@
+//author @Nishant
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Matrix{
+    public:
+        vector<vector<int>> a;
+        Matrix& operator +(Matrix x){
+            Matrix res;
+            for(int i=0; i<x.a.size(); i++){
+                for(int j=0; j<x.a[0].size(); j++){
+                    this->a[i][j] += x.a[i][j];
+                }
+            }
+            return *this;
+        }
+};
+
+int main()
+{
+    int cases, k;
+    cin >> cases;
+    for (k = 0; k < cases; k++)
+    {
+        Matrix x;
+        Matrix y;
+        Matrix result;
+        int n, m, i, j;
+        cin >> n >> m;
+        for (i = 0; i < n; i++)
+        {
+            vector<int> b;
+            int num;
+            for (j = 0; j < m; j++)
+            {
+                cin >> num;
+                b.push_back(num);
+            }
+            x.a.push_back(b);
+        }
+        for (i = 0; i < n; i++)
+        {
+            vector<int> b;
+            int num;
+            for (j = 0; j < m; j++)
+            {
+                cin >> num;
+                b.push_back(num);
+            }
+            y.a.push_back(b);
+        }
+        result = x + y;
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < m; j++)
+            {
+                cout << result.a[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
